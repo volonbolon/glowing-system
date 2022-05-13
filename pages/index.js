d9a77c544f6533a9c3207b7685d23d2c23dcde8f
Ariel Rodriguez <ariel@volonbolon.me>
2022-05-13 16:32:04 -0300

    layout index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
 import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -22,11 +23,19 @@ class CampaignIndex extends Component {
   }
 
   render() {
-    return <div>
-      <link async rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css" />
-      {this.renderCampaigns()}
-      <Button content="Create Campaign" icon="add circle" primary/>
-      </div>;
+    return (
+      <Layout>
+        <div>
+        <link async rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css" />
+        <h3>Open Campaigns</h3>
+        <Button floated="right"
+         content="Create Campaign" 
+         icon="add circle" 
+         primary/>
+        {this.renderCampaigns()}
+        </div>
+      </Layout>
+    );
   }
 }
 
